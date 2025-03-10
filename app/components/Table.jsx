14bd6f8019bcef6ea73d61001823dc283dd47b34
Martin Lulgjuraj <martinlulgjuraj@martins-mbp.mynetworksettings.com>
2025-03-10 14:05:08 -0400

    started favlinks project files

---
+++ app/components/Table.jsx
@@ -0,0 +1,20 @@
+
+
+function Table(){
+
+    return(
+        // Code will go here
+        <table>
+            {/* Table header will go here*/}
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>URL</th>
+                </tr>
+            </thead>
+        </table>
+    )
+
+}
+
+export default Table
--- app/page.jsx
+++ app/page.jsx
@@ -7,18 +7,21 @@
 import { useState } from "react" // we use this to add state in our components
 // state lets us keep track of changing data and show it in the component
 
+import Form from "./components/Form"
+import Table from "./components/Table"
+
 function  HomePage(){
-
-    // create some state keeping track of the changing const
-    const [count, setCount] = useState(0)
-
     return (
         <div>
-            <h1>Counter: {count}</h1>
-            <button onClick={()=>{
-                // write some Javascript for this function to call
-                setCount(count + 1)
-            }}> Add One </button>
+            <h1> Fav Links </h1>
+
+            <Form />
+
+            {/* A table the user can use to see their submissions */}
+            <Table />
+
+
+            
         </div>
     )
 }
